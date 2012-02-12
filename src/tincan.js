afc1cbe4d9097daa3469cff540010b8d52c3ea28
Bryan Grohman <bgrohman@gmail.com>
2012-02-12 15:42:33 -0600

    added unsubscribe

--- src/tincan.js
+++ src/tincan.js
@@ -5,22 +5,26 @@
  */
 (function(globals) {
 
-    var subscribers = [];
+    var subscribers = [],
+        subscriberId = 1;
 
     /**
      * Publishes a message.
      * @param msg the message to publish
      */
     function publish(msg) {
-        // TODO: arg support...
-        var toBeNotified = subscribers[msg],
+        var subscriberId,
             subscriber,
-            context,
-            i,
-            len;
-        
-        for (i = 0, len = toBeNotified.length; i < len; i++) {
-            toBeNotified[i].callback.apply(toBeNotified[i].context);
+            msgSubscribers = subscribers[msg],
+            hasOwnProperty = Object.prototype.hasOwnProperty;
+
+        if (msgSubscribers) {
+            for (subscriberId in msgSubscribers) {
+                if (hasOwnProperty.call(msgSubscribers, subscriberId)) {
+                    subscriber = msgSubscribers[subscriberId];
+                    subscriber.callback.apply(subscriber.context);
+                }
+            }
         }
     }
 
@@ -32,27 +36,54 @@
      * @return true if the subscription was successfull
      */
     function subscribe(msg, callback, context) {
-        if (typeof callback === 'function') {
-            var subscriber = {
-                callback: callback,
-                context: context || null
-            };
-
-            if (typeof subscribers[msg] === 'undefined') {
-                subscribers[msg] = [];
-            }
-
-            subscribers[msg].push(subscriber);
-
-            return true;
+        if (typeof callback !== 'function') {
+            return undefined;
         }
 
-        return false;
+        var subscriber = {
+            callback: callback,
+            context: context || null
+        };
+
+        if (typeof subscribers[msg] === 'undefined') {
+            subscribers[msg] = {};
+        }
+
+        subscribers[msg][subscriberId] = subscriber;
+
+        return subscriberId++;
+    }
+
+    /**
+     * Unsubscribe the given subscriber id from the given message.
+     * @param msg the message to unsubscribe from
+     * @param subscriberId the subscriber id to unsubscribe
+     */
+    function unsubscribe(msg, subscriberId) {
+        if (subscribers[msg]) {
+            delete subscribers[msg][subscriberId];
+        }
+    }
+
+    /**
+     * Unsubscibes all subscribers from the given message
+     * or unsubscribes all subscribers from all messages
+     * if no message is given.
+     * @param msg (optional) the message to unsubscribe from
+     */
+    function unsubscribeAll(msg) {
+        if (typeof msg === 'undefined') {
+            subscribers = [];
+        } else {
+            delete subscribers[msg];
+        }
     }
 
     globals.tincan = {
         publish: publish,
-        subscribe: subscribe
+        subscribe: subscribe,
+        unsubscribe: unsubscribe,
+        unsubscribeAll: unsubscribeAll
     };
 
 })(window);
